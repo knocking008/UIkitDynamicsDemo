c305de6c11e41cb2aa303ba4d9948780be1abf3c
杨植生 <vincent.yang@jinfuzi.com>
2016-09-05 09:30:22 +0800

    Initial Commit

---
+++ UIKitDynamicsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIKitDynamicsDemo
+//
+//  Created by vincent on 16/9/5.
+//  Copyright © 2016年 FuTouBang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
